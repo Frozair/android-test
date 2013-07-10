b45fb20f5c8da42ac227cf523a5a489aeaa2a9ed
Rudolph Newball <rudolph.newball@gmail.com>
2013-07-10 14:11:24 -0500

    Initial commit with first two tests

---
+++ AndroidBasicsStarter/src/main/java/com/frozair/androidgames/AndroidBasicsStarter.java
@@ -0,0 +1,41 @@
+package com.frozair.androidgames;
+
+import android.*;
+import android.R;
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AndroidBasicsStarter extends ListActivity {
+    String tests[] = { "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
+                      "KeyTest", "AccelerometerTest", "AssetsTest",
+                      "ExternalStorageTest", "SoundPool", "MediaPlayerTest",
+                      "FullScreenTest", "RenderViewTest", "ShapeTest", "BitmapTest",
+                      "FontTest", "SurfaceViewTest" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.simple_list_item_1, tests));
+    }
+
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id)
+    {
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+        try
+        {
+            Class clazz = Class.forName("com.frozair.androidgames." + testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
